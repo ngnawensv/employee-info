5aee40e8553d84d21a16de1bf043d645173cdc04
Ngnawen Samuel <ngnawens@gmail.com>
2021-08-20 21:42:52 +0400

    first commit

---
+++ src/main/kotlin/cm/belrose/employeeinfo/EmployeeInfoApplication.kt
@@ -0,0 +1,11 @@
+package cm.belrose.employeeinfo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EmployeeInfoApplication
+
+fun main(args: Array<String>) {
+    runApplication<EmployeeInfoApplication>(*args)
+}
---
+++ src/test/kotlin/cm/belrose/employeeinfo/EmployeeInfoApplicationTests.kt
@@ -0,0 +1,13 @@
+package cm.belrose.employeeinfo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EmployeeInfoApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
